5ab52483472100eea50e434ad2b124f3bc4bac0a
din_alt@poczta.onet.pl <din_alt@poczta.onet.pl>
2020-05-17 11:08:05 +0200

    Game board ready.

--- Reversi/Engine.cs
+++ Reversi/Engine.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace Reversi
 {
-    class Engine
+    public class Engine
     {
+        public int BoardWidth { get; private set; }
+        public int BoardHeight { get; private set; }
+
+        private int[,] board;
+        public int NextPlayerNumber { get; private set; } = 1;
+
+        private static int OponentNumber( int playerNumber)
+        {
+            return (playerNumber == 1) ? 2 : 1;
+        }
+        private bool areCoordinatesCorrect(int width, int height)
+        {
+            return width >= 0 && width < BoardWidth &&
+                height >= 0 && height < BoardHeight;
+        }
+        public int GetFieldState(int width, int height)
+        {
+            if(!areCoordinatesCorrect(width, height))
+            {
+                throw new Exception("Nieprawidłowe współrzędne pola!");
+            }
+            return board[width, height];
+        }
+
+        private void ClearBoard()
+        {
+            for(int i =0; i<BoardWidth; i++)
+            {
+                for(int j = 0; j < BoardHeight; j++)
+                {
+                    board[i, j] = 0;
+                }
+            }
+            int middleWidth = BoardWidth / 2;
+            int middleHeight = BoardHeight / 2;
+            board[middleHeight - 1, middleWidth - 1] = board[middleWidth, middleHeight] = 1;
+            board[middleWidth - 1, middleHeight] = board[middleWidth, middleHeight - 1] = 2;
+        }
+        public Engine( int initialPlayer, int boardWidth = 8, int boardHeight = 8)
+        {
+            if(initialPlayer < 1 || initialPlayer > 2)
+            {
+                throw new Exception("Nieprawidłowy numer gracza rozpoczynającego grę!");
+            }
+            BoardWidth = boardWidth;
+            BoardHeight = boardHeight;
+            board = new int[BoardWidth, BoardHeight];
+            ClearBoard();
+            NextPlayerNumber = initialPlayer;
+        }
     }
 }
